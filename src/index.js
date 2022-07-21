3d83146aa245ac744ad858c1402763c4cff9e72d
Aleksandra Ujvari <ujvari65@gmail.com>
2022-07-21 11:58:14 +0200

    Update index.js file, and add changes to css and html file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const refresh = document.querySelector('.btn-refresh');
+const leadersName = document.querySelector('leaders-name');
+const leadersScore = document.querySelector('leaders-score');
+const btnSubmit = document.querySelector('btn-submit');
+const listScore = document.querySelector('listscore');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['', ''], ' ');
+const gameId = 'Ni1OxKmEJWvyYe0xcLQq';
 
-  return element;
-}
+const submitScore = async (userName, userScore) => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+    headers: {
+      'Content-Type': 'application/json', charset:"UTF-8",
+    },
+  });
+  const gameResult = await response.json();
+  return gameResult;
+};
 
-document.body.appendChild(component());
+const fetchDataFromAPI = async () => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`);
+  const getResult = await response.json();
+  const resultArray = getResult.result;
+  const values = resultArray.map((result) => `<li class="display-table-list">
+                      <p>${result.user}: ${result.score}</p>
+                  </li>`).join('');
+  listScore.innerHTML = values;
+};
+
+btnSubmit.addEventListener('click', async () => {
+  await submitScore(leadersName.value, leadersScore.value);
+  leadersName.value = '';
+  leadersScore.value = '';
+  fetchData();
+});
+
+refresh.addEventListener('click', async () => {
+  fetchData();
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+  fetchData();
+});
